e47de8098d586342dbad26aa18f47b8e114f438a
Jeroen <Jeroen_delger@hotmail.com>
2024-11-19 13:31:39 +0100

    Black Jack 2.0

--- resources/js/views/blackjack.vue
+++ resources/js/views/blackjack.vue
@@ -1,33 +1,55 @@
 <template>
     <div>
-        <button v-if="PlayerCards.length == 0" @click="StartGame()">
-            Start Game
-        </button>
-        <button v-if="PlayerCards.length > 0" @click="GetCard()">
-            Geef extra Kaart
-        </button>
-        <button v-if="PlayerCards.length > 0" @click="Dealer()">
-            Houd Kaarten/Dealer
-        </button>
+        <div v-if="GameBoard == false">
+            Aantal Players:<select v-model.number="AantalPlayers">
+                <option disabled value="">Hoeveel Spelers</option>
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+            </select>
+            <button @click="StartGame()">
+                Start Game
+            </button>
+        </div>
+
+        <div v-if="GameBoard == true" >
         <br/>
-        Player:
-        <table>
-            <th v-for="PlayerCard in PlayerCards" :style="{width:'66px', height:'100px', border:'1px solid black'}">
-                {{ PlayerCard.Card }}
-                {{ PlayerCard.Symbol }}
-            </th>
-        </table>
-        Player Score: {{ PlayerPoints }} <br/>
-    <br/>
-        Dealer:
-        <table>
-            <th v-for="DealerCard in DealerCards" :style="{width:'66px', height:'100px', border:'1px solid black'}">
-                {{ DealerCard.Card }}
-                {{ DealerCard.Symbol }}
-            </th>
-        </table>
-        Dealer Score: {{ DealerPoints }} <br/>
-        Winnaar: {{ Winnaar }}
+            <table v-for="Player in AantalPlayers" :style="{visibility:GameBoard}">
+                Player:{{Player}}<br/>
+                Score: 
+                <div v-if="PlayerPoints[Player-1] > 0">
+                    {{ PlayerPoints[Player-1] }} 
+                </div>
+                <div v-else="">Defeat</div>
+                <br/>
+                <th v-for="PlayerCard in PlayersCards[Player-1]" :style="{width:'66px', height:'100px', border:'1px solid black'}">
+                    {{ PlayerCard.Card }}
+                    {{ PlayerCard.Symbol }}
+                </th>
+                <div v-if="Player == ActivePlayer+1 && PlayerHolds[ActivePlayer] == false" >                    
+                    <button @click="GetCard()">
+                        Geef extra Kaart
+                    </button>
+                    <button @click="HoldCard()">
+                        Houd Kaarten
+                    </button>
+                </div>
+                    <br/>
+            </table>
+            <br/>
+            <div v-if="PlayerHolds.every(Boolean) ==  true">
+                <table>
+                    Dealer: <br/>
+                    Dealer Score: {{ DealerPoints }} <br/>
+                    <th v-for="DealerCard in PlayersCards[AantalPlayers]" :style="{width:'66px', height:'100px', border:'1px solid black'}">
+                        {{ DealerCard.Card }}
+                        {{ DealerCard.Symbol }}
+                    </th>
+                </table>
+                Winnaar: {{ Winnaar }}
+            </div>
+        </div>
     </div>
 </template>
 
@@ -39,15 +61,35 @@ import { ref } from 'vue'
     let Cards = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King'];
     let Points = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10];
     let Deck = ref([]);
-    let PlayerCards = ref([])
-    let DealerCards = ref([])
     let Winnaar = ref('')
-    let PlayerPoints = ref(0)
+    let PlayerPoints = ref([])
     let DealerPoints = ref(0)
-    let AllePuntenPlayer = ref([])
-    let AllePuntenDealer = ref([])
-    let PlayerAce = ref([0])
-    let DealerAce = ref([0])
+    let AllePunten = ref([[],[],[],[],[]])
+    let Aces = ref([[],[],[],[],[]])
+    let AantalPlayers = ref(3)
+    let PlayersCards = ref([[],[],[],[],[]])
+    let PlayerHolds = ref([])
+    let AantalKaarten = ref([])
+    let GameBoard = ref(false)
+    let ActivePlayer = ref(0)
+
+    function HoldCard(){
+        PlayerHolds.value[ActivePlayer.value] = true
+        NextPlayer()
+    }
+
+    function NextPlayer(){
+        ActivePlayer.value++
+        if (ActivePlayer.value > AantalPlayers.value){
+            ActivePlayer.value = 0
+        }
+        if(PlayerHolds.value[ActivePlayer.value] == true && PlayerHolds.value.every(Boolean) == false || PlayerHolds.value[ActivePlayer.value] == undefined ){
+            NextPlayer()
+        }
+        else if(PlayerHolds.value.every(Boolean) ==  true ){
+            PuntenBerekeningDealer()
+        }
+    }
 
     function Playingcards(){
         for(let i = 0; i < Suits.length; i++){
@@ -57,90 +99,123 @@ import { ref } from 'vue'
                 Deck.value.push(card);
             }
         }
+        StartingHand()
         return Deck.value
     }
 
     function StartGame(){
         Playingcards()
-        GetCard()
-        GetCard()
+    }
+
+    function StartingHand(){
+        for(let i = 0; i < AantalPlayers.value+1; i++){
+            AantalKaarten.value[i] = 0
+            PlayersCards.value[i][AantalKaarten.value[i]] = Deck.value.splice(Math.floor(Math.random()*Deck.value.length), 1)[0]
+            AantalKaarten.value[i]++
+            PlayersCards.value[i][AantalKaarten.value[i]] = Deck.value.splice(Math.floor(Math.random()*Deck.value.length), 1)[0]
+            AantalKaarten.value[i]++
+            if(i <= AantalPlayers.value-1){
+                PlayerHolds.value[i] = false
+            }
+            PuntenBerekeningPlayer()
+        }
+        GameBoard.value = true
     }
 
     function GetCard(){
-        PlayerCards.value[PlayerCards.value.length] = Deck.value.splice(Math.floor(Math.random()*Deck.value.length), 1)[0];
+        PlayersCards.value[ActivePlayer.value][AantalKaarten.value[ActivePlayer.value]] = Deck.value.splice(Math.floor(Math.random()*Deck.value.length), 1)[0];
+        AantalKaarten.value[ActivePlayer.value]++
         PuntenBerekeningPlayer()
     }
 
     function PuntenBerekeningPlayer(){
-        PlayerPoints.value = 0
-        for(let i = 0; i < PlayerCards.value.length; i++){
-            if (PlayerCards.value[i].Points < 11){
-                AllePuntenPlayer.value[i] = PlayerCards.value[i].Points
+        PlayerPoints.value[ActivePlayer.value] = 0
+        for(let i = 0; i < AantalKaarten.value[ActivePlayer.value]; i++){            
+            if (PlayersCards.value[ActivePlayer.value][i].Points < 11){
+                AllePunten.value[ActivePlayer.value][i] = PlayersCards.value[ActivePlayer.value][i].Points
             }
-            if (PlayerCards.value[i].Points > 10){
-                AllePuntenPlayer.value[i] = 0
-                PlayerCards.value[i].Points = 0
-                PlayerAce.value++                
+            if (PlayersCards.value[ActivePlayer.value][i].Points > 10){
+                AllePunten.value[ActivePlayer.value][i] = 0
+                PlayersCards.value[ActivePlayer.value][i].Points = 0
+                Aces.value[ActivePlayer.value]++                
             }
-            PlayerPoints.value = PlayerPoints.value + AllePuntenPlayer.value[i]
+            PlayerPoints.value[ActivePlayer.value] = PlayerPoints.value[ActivePlayer.value] + AllePunten.value[ActivePlayer.value][i]
         }
-        if (PlayerAce.value > 0 && PlayerPoints.value + 10 + PlayerAce.value < 22){
-            PlayerPoints.value = PlayerPoints.value + 10 + PlayerAce.value
+
+        if (Aces.value[ActivePlayer.value] > 0 && PlayerPoints.value[ActivePlayer.value] + 10 + Aces.value[ActivePlayer.value] < 22){
+            PlayerPoints.value[ActivePlayer.value] = PlayerPoints.value[ActivePlayer.value] + 10 + Aces.value[ActivePlayer.value]
         }
-        else if (PlayerAce.value > 0 && PlayerPoints.value + 10 + PlayerAce.value > 21){
-            PlayerPoints.value = PlayerPoints.value + PlayerAce.value
+
+        else if (Aces.value[ActivePlayer.value] > 0 && PlayerPoints.value[ActivePlayer.value] + 10 + Aces.value[ActivePlayer.value] > 21){
+            PlayerPoints.value[ActivePlayer.value] = PlayerPoints.value[ActivePlayer.value] + Aces.value[ActivePlayer.value]
         }            
         
-        if(PlayerPoints.value == 21 && PlayerCards.value.length == 2){
-            Winnaar.value = 'Player'
-            return window.alert("Je hebt Gewonnen!")
+        if(PlayerPoints.value[ActivePlayer.value] > 21){
+            PlayerPoints.value[ActivePlayer.value] = 0
+            PlayerHolds.value[ActivePlayer.value] = true
+            window.alert("Player "+(ActivePlayer.value+1)+" heeft Verloren!")
+            return NextPlayer()
         }
-
-        else if(PlayerPoints.value > 21){
-            Winnaar.value = 'Dealer'
-            return window.alert("Je hebt Verloren!")
-        }
-    }
-
-    function Dealer(){
-        DealerCard()        
+        NextPlayer()
     }
 
     function DealerCard(){
-        DealerCards.value[DealerCards.value.length] = Deck.value.splice(Math.floor(Math.random()*Deck.value.length), 1)[0];
+        PlayersCards.value[AantalPlayers.value][AantalKaarten.value[AantalPlayers.value]] = Deck.value.splice(Math.floor(Math.random()*Deck.value.length), 1)[0];
+        AantalKaarten.value[AantalPlayers.value]++
         PuntenBerekeningDealer()
     }
-
-
+    
+    
     function PuntenBerekeningDealer(){
         DealerPoints.value = 0
-        for(let i = 0; i < DealerCards.value.length; i++){
-            if (DealerCards.value[i].Points < 11){
-                AllePuntenDealer.value[i] = DealerCards.value[i].Points
+        for(let i = 0; i < AantalKaarten.value[AantalPlayers.value]; i++){            
+            if (PlayersCards.value[AantalPlayers.value][i].Points < 11){
+                AllePunten.value[AantalPlayers.value][i] = PlayersCards.value[AantalPlayers.value][i].Points
             }
-            if (DealerCards.value[i].Points > 10){
-                AllePuntenDealer.value[i] = 0
-                DealerCards.value[i].Points = 0
-                DealerAce.value++                
+            if (PlayersCards.value[AantalPlayers.value][i].Points > 10){
+                AllePunten.value[AantalPlayers.value][i] = 0
+                PlayersCards.value[AantalPlayers.value][i].Points = 0
+                Aces.value[AantalPlayers.value]++                
             }
-            DealerPoints.value = DealerPoints.value + AllePuntenDealer.value[i]
+            DealerPoints.value = DealerPoints.value + AllePunten.value[AantalPlayers.value][i]
         }
-        if (DealerAce.value > 0 && DealerPoints.value + 10 + DealerAce.value < 22){
-            DealerPoints.value = DealerPoints.value + 10 + DealerAce.value
+
+        if (Aces.value[AantalPlayers.value] > 0 && DealerPoints.value + 10 + Aces.value[AantalPlayers.value] < 22){
+            DealerPoints.value = DealerPoints.value + 10 + Aces.value[AantalPlayers.value]
         }
-        else if (DealerAce.value > 0 && DealerPoints.value + 10 + DealerAce.value > 21){
-            DealerPoints.value = DealerPoints.value + DealerAce.value
-        } 
-        if (DealerPoints.value > 21){
-            Winnaar.value = 'Player'
-            return window.alert("Je hebt gewonnen!")
+        else if (Aces.value[AantalPlayers.value] > 0 && DealerPoints.value + 10 + Aces.value[AantalPlayers.value] > 21){
+            DealerPoints.value = DealerPoints.value + Aces.value[AantalPlayers.value]
+        }            
+                   
+        if (DealerPoints.value < Math.max(...PlayerPoints.value)
+        ){
+           return DealerCard()
         }
-        else if (DealerPoints.value > PlayerPoints.value){
+        else return WinnaarCheck()
+    }
+
+    function WinnaarCheck(){
+        if (DealerPoints.value > Math.max(...PlayerPoints.value)
+        && DealerPoints.value < 22
+        ){
             Winnaar.value = 'Dealer'
-            return window.alert("Je hebt Verloren!")
+            return window.alert("De dealer heeft gewonnen!")
         }
-        else if (DealerPoints.value <= PlayerPoints.value){
-            DealerCard()
+
+        else if(PlayerPoints.value.filter((item, index) => PlayerPoints.value.indexOf(item) !== index) >= [Math.max(...PlayerPoints.value)]){
+           Winnaar.value = 'Gelijk spel'
+           return window.alert("Het is gelijkspel")
+        }
+            
+        else if (DealerPoints.value > 21 
+        && Math.max(...PlayerPoints.value) < 22){
+            Winnaar.value = ("Player "+ (PlayerPoints.value.indexOf(Math.max(...PlayerPoints.value))+1))
+            return window.alert("Player "+(PlayerPoints.value.indexOf(Math.max(...PlayerPoints.value))+1)+" heeft Gewonnen!")
+        }
+        
+        else if (DealerPoints.value == Math.max(...PlayerPoints.value)){
+            Winnaar.value = 'Gelijk spel'
+            return window.alert("Het is gelijkspel")
         }
     }
 
